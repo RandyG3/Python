a85f409164f1a3fcb58624738225eaccfc7b968b
RandyG3 <40631249+RandyG3@users.noreply.github.com>
2020-01-28 16:21:01 -0500

    EOD 1-28-20

---
+++ 05-Functions/intro-to-functions.py
@@ -0,0 +1,7 @@
+def output_text(): # this is minimum for def definition
+    print("Welcome to the program")
+    print("It's nice to meet you")
+    print("Have an excellent day programming!")
+
+output_text()
+output_text()
