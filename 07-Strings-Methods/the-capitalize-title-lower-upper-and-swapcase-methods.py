d45aeab167585a64bc86a779d575a8ac23cb9760
RandyG3 <40631249+RandyG3@users.noreply.github.com>
2020-01-30 16:09:18 -0500

    end of day 1-30-20

---
+++ 07-Strings-Methods/the-capitalize-title-lower-upper-and-swapcase-methods.py
@@ -0,0 +1,14 @@
+story = "Once upon a time"
+
+print(story.capitalize())
+print(story.title())
+print(story.upper())
+print("HELLO".lower())
+print(story.swapcase()) # all lower to upper & all upper to lower
+print("AbCdE".swapcase())
+
+# method chaining - make the name proper case
+print("RANDY GALINAT".lower().title())
+
+story = story.title() + "0"
+print(story)
