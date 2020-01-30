748e5a69cf887f2cf97fa509e2cb6e28b1be3df0
RandyG3 <40631249+RandyG3@users.noreply.github.com>
2020-01-30 14:01:24 -0500

    Section 6 done 1-30-20

--- 06-Strings-The-Basics/string-indexing-with-negative-values.py
+++ 06-Strings-The-Basics/string-indexing-with-negative-values.py
@@ -2,7 +2,7 @@ best_language_ever = "Python"
 
 print(type((best_language_ever[0])))
 
-print(best_language_ever[-1])
+print(best_language_ever[:-1])
 print(best_language_ever[-2])
 print(best_language_ever[-3])
 print(best_language_ever[-4])
