c8e02c98850fff1b5898fbe8d8a2b8aca17fd35c
RandyG3 <40631249+RandyG3@users.noreply.github.com>
2020-01-31 16:22:04 -0500

    EOD 1-31-20

---
+++ 08-Control-Flow/even_or_odd.py
@@ -0,0 +1,22 @@
+# Define a even_or_odd function that accepts a single integer.
+# If the integer is even, the function should return the string “even”.
+# If the integer is odd, the function should return the string “odd”.
+#
+# even_or_odd(2) => "even"
+# even_or_odd(0) => "even"
+# even_or_odd(13) => "odd"
+# even_or_odd(9) => "odd"
+
+
+# def even_or_odd(intnum):
+#     even_odd = "Odd"
+#     if intnum % 2:
+#         even_odd = "Even"
+#         return even_odd
+
+#print(even_or_odd(2))        
+
+if 2 % 2:
+    print("True")
+
+print(2 % 2)    
---
+++ 08-Control-Flow/truthy_or_falsy.py
@@ -0,0 +1,9 @@
+# Define a truthy_or_falsy function that accepts a single argument.
+# The function should return a string that reads "The value _____ is ______"
+# where the first space is the argument and the second space
+# is either truthy or falsy.
+#
+# truthy_or_falsy(0) => "The value 0 is falsy"
+# truthy_or_falsy(5) => "The value 5 is truthy"
+# truthy_or_falsy("Hello") => "The value Hello is truthy"
+# truthy_or_falsy("") => "The value  is falsy"
