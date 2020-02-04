2febbbb710395f66140e04311b06052def62bac1
“RandyG3” <tbear03@comcast.net>
2020-02-03 20:58:47 -0500

    Finished at home

--- 08-Control-Flow/the-and-keyword.py
+++ 08-Control-Flow/the-and-keyword.py
@@ -3,3 +3,12 @@ if 5 < 7 and "rain" == "rain":
 
 if 5 < 7 and "rain" == "fire":
     print("at least one is False, this code doesn't execute")
+
+# Python checks the 1st condition and if false, immediately stops the evaluation
+
+value = 95
+
+# if value > 90 and value < 100:
+# Below is a better way of coding
+if 90 < value < 100:
+    print("This is a shortcut for the win!")
