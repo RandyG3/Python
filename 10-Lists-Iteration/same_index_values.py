ab9653c68b2cd1f9b9a74768ad552f7a79d39ceb
RandyG3 <40631249+RandyG3@users.noreply.github.com>
2020-02-06 16:17:08 -0500

    EOD 02-06-20

---
+++ 10-Lists-Iteration/same_index_values.py
@@ -0,0 +1,5 @@
+# Declare a function same_index_values that accepts two lists.
+# The function should return a list of the index positions in which the two lists have equal elements
+#
+# same_values([1, 2, 3], [3, 2, 1]) => [1]
+# same_values(["a", "b", "c", "d"], ["c", "b", "a", "d"]) => [1, 3]
