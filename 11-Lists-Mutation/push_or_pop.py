5c68f65b8fc929b90946e93d14c32081b3ae6a9a
RandyG3 <40631249+RandyG3@users.noreply.github.com>
2020-02-10 16:18:01 -0500

    EOD 02-10-20

---
+++ 11-Lists-Mutation/push_or_pop.py
@@ -0,0 +1,10 @@
+# Declare a push_or_pop function that accepts a list of numbers
+# Build up and return a new list by iterating over the list of numbers
+# If a number is greater than 5, add it to the end of the new list
+# If a number is less than or equal to 5, remove the last element added to the new list
+# Assume the order of numbers in the argument will never require removing from an empty list
+#
+# push_or_pop([10]) => [10]
+# push_or_pop([10, 4]) => []
+# push_or_pop([10, 20, 30]) => [10, 20, 30]
+# push_or_pop([10, 20, 2, 30]) => [10, 30]
---
+++ 11-Lists-Mutation/while_testing.py
@@ -0,0 +1,8 @@
+def delete_all(strings, target):
+    i = 0
+
+
+    return strings
+
+
+print(delete_all([5, 3, 5], 5))
