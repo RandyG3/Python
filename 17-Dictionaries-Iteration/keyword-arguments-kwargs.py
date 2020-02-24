45e4531fc8e12e3620b7b450737de9ef32d73dae
RandyG3 <40631249+RandyG3@users.noreply.github.com>
2020-02-24 16:14:29 -0500

    EOD 02-24-20

--- 17-Dictionaries-Iteration/keyword-arguments-kwargs.py
+++ 17-Dictionaries-Iteration/keyword-arguments-kwargs.py
@@ -3,7 +3,7 @@ def length(word):
 
 
 print(length("Hello"))
-print(length(word="Hello"))
+print(length(word="Hello"))  # Keyword arg used
 
 # print(length())  # Returns error
 # print(length(something = "Hello"))  # what is something? error returned
@@ -11,6 +11,7 @@ print(length(word="Hello"))
 
 
 def collect_keyword_arguments(**kwargs):  # kwargs is conventional name
+    # **kwargs creates a dictionary with k/v pairs
     print(kwargs)
     print(type(kwargs))
 
@@ -19,15 +20,19 @@ def collect_keyword_arguments(**kwargs):  # kwargs is conventional name
 
 
 collect_keyword_arguments(a=2, b=3, c=4, d=5)
-#
-# def args_and_kwargs(a, b, *args, **kwargs):
-#     print(f"The total of your regular arguments a and b is {a + b}")
-#     print(f"The total of your args tuple is {sum(args)}")
-#
-#     dict_total = 0
-#     for value in kwargs.values():
-#         dict_total += value
-#
-#     print(f"The total of your kwargs dictionary is {dict_total}")
-#
-#args_and_kwargs(1, 2, 3, 4, 5, 6, x = 8, y = 9, z = 10)
+
+
+def args_and_kwargs(a, b, *args, **kwargs):
+    #               the above is the order for mixed input
+    print(f"The total of your regular arguments a and b is {a + b}")
+    print(f"The total of your args tuple is {sum(args)}")
+
+    dict_total = 0
+    for value in kwargs.values():
+        dict_total += value
+
+    print(f"The total of your kwargs dictionary is {dict_total}")
+
+
+#               a  b  ..tuple...  kwargs
+args_and_kwargs(1, 2, 3, 4, 5, 6, x=8, y=9, z=10)
